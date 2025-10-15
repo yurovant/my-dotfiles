4114d8b8317baff1df323361bbed6e01ecb50a07
Anton Yurovskykh (Valtech) <anton.yurovskykh@valtech.com>
2025-10-15 17:50:42 +0300

    Update

--- Home/.config/nvim/lua/remapping.lua
+++ Home/.config/nvim/lua/remapping.lua
@@ -6,67 +6,55 @@ local modes = {"n", "i", "v", "c"}
 
 for _, key in ipairs(disabled_keys) do
     -- The "" (empty string) as the right-hand side acts like <Nop>
-    vim.keymap.set(modes, key, "", {
-        noremap = true
-    })
+    vim.keymap.set(modes, key, "")
 end
 
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- ==================================================================
 -- NORMAL and VISUAL modes
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- ==================================================================
 -- "M" to jump to the beginning of the line (original "0" behavior)
-vim.keymap.set({"n", "v"}, "M", "0", {
-    noremap = true
-})
-
+vim.keymap.set({"n", "v"}, "M", "0")
 -- "L" for jump to the end of the line (original "$" behavior)
-vim.keymap.set({"n", "v"}, "L", "$", {
-    noremap = true
-})
+vim.keymap.set({"n", "v"}, "L", "$")
 
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- ==================================================================
 -- VISUAL and INSERT modes
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-vim.keymap.set({"v", "i"}, "kj", "<Esc>", {
-    noremap = true
-})
+-- ==================================================================
+vim.keymap.set({"v", "i"}, "kj", "<Esc>")
 
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- ==================================================================
 -- NORMAL mode
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- ==================================================================
+-- 
 -- left
-vim.keymap.set("n", "m", "h", {
-    noremap = true
-})
-
+vim.keymap.set("n", "m", "h")
 -- down
-vim.keymap.set("n", ",", "j", {
-    noremap = true
-})
-
+vim.keymap.set("n", ",", "j")
 -- "h" for marks instead of (original "m" behavior)
-vim.keymap.set("n", "h", "m", {
-    noremap = true
-})
-
+vim.keymap.set("n", "h", "m")
 -- "j" for reverse t/T/f/F instead of (original "," behavior)
-vim.keymap.set("n", "j", ",", {
-    noremap = true
-})
+vim.keymap.set("n", "j", ",")
 
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- Center the screen when jumping
+-- ==================================================================
+-- Next search result
+vim.keymap.set("n", "n", "nzzzv")
+-- Previous search result
+vim.keymap.set("n", "N", "Nzzzv")
+-- Half page down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- Half page up
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- ==================================================================
 -- VISUAL mode
--- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- ==================================================================
 -- left
-vim.keymap.set("v", "m", "h", {
-    noremap = true
-})
-
+vim.keymap.set("v", "m", "h")
 -- down
-vim.keymap.set("v", ",", "j", {
-    noremap = true
-})
+vim.keymap.set("v", ",", "j")
 
+-- ==================================================================
 -- Issues to fix:
 
 -- 1. You're missing the j → , remap in Visual mode
