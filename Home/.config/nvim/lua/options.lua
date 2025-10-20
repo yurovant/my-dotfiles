ed4d46c8258c2c49b7121d4721a6748713fc7f84
Anton Yurovskykh (Valtech) <anton.yurovskykh@valtech.com>
2025-10-20 12:53:50 +0300

    Update

--- Home/.config/nvim/lua/options.lua
+++ Home/.config/nvim/lua/options.lua
@@ -1,52 +1,35 @@
 local opt = vim.opt
 
--- Line numbers
--- show line numbers
-opt.number = true
--- show relative line numbers
-opt.relativenumber = true
--- keep the numbers column narrow
-opt.numberwidth = 2
--- highlight current line
-opt.cursorline = true
--- keep 10 lines above / below the cursor
-opt.scrolloff = 10
-opt.sidescrolloff = 8
--- keep 8 columns left / right of the cursor (wut?)
-
--- wrap the lines (true is default)
--- opt.wrap = false
+-- Lines
+opt.number = true                -- Display absolute line numbers
+opt.relativenumber = true        -- Show line numbers relative to cursor position
+opt.numberwidth = 2              -- Width of the line number column
+opt.cursorline = true            -- Highlight the current line
+opt.scrolloff = 10               -- Keep 10 lines visible above/below cursor when scrolling
 
 -- Indentation
--- tab width
-opt.tabstop = 2
--- indent width
-opt.shiftwidth = 2
--- soft tab stop (wut?)
-opt.softtabstop = 2
--- use spaces instead of tabs
-opt.expandtab = true
--- smart auto-indentation
-opt.smartindent = true
--- copy indentation from current line
-opt.autoindent = true
+opt.tabstop = 2                  -- Display tab characters as 2 spaces wide
+opt.shiftwidth = 2               -- Use 2 spaces for automatic indentation (>>, <<, etc.)
+opt.softtabstop = 2              -- Treat 2 spaces as a tab unit when editing
+opt.expandtab = true             -- Insert spaces instead of tab characters
+opt.smartindent = true           -- Automatically indent after opening braces
+opt.autoindent = true            -- Copy indentation from previous line
 
 -- Search
-opt.ignorecase = true
-opt.smartcase = true
-opt.incsearch = true
+opt.ignorecase = true            -- Search is case-insensitive by default
+opt.smartcase = true             -- Override ignorecase if search contains uppercase letters
+opt.incsearch = true             -- Highlight matches as you type the search pattern
 
 -- don't highlight search results (why?)
 -- opt.hlsearch = false
 
--- UI
--- opt.termguicolors = true
--- always show sign column (wut?)
-opt.signcolumn = "yes"
-opt.colorcolumn = "100"
-opt.showmatch = true
-opt.matchtime = 2
--- opt.updatetime = 50
+-- Visual settings
+opt.termguicolors = true         -- Enable true color (24-bit RGB) support for vibrant colors
+opt.signcolumn = "yes"           -- Always show the sign column for git/diagnostic indicators
+opt.showmatch = true             -- Briefly highlight matching brackets when cursor is on one
+opt.matchtime = 2                -- Duration (in tenths of a second) to highlight matching brackets
+opt.cmdheight = 1                -- Height of the command line at the bottom
+opt.updatetime = 50              -- Milliseconds before swap file is written and CursorHold fires
 
 -- File handling
 opt.undofile = true
